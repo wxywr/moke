10ab211ffd6b4e864b88210c8804e663324b2a2b
wxywr <1261046195@qq.com>
2017-11-28 13:44:17 +0800

    update

--- js/index.js
+++ js/index.js
@@ -12,5 +12,32 @@ $(function(){
 		$(this).parent().parent().parent().css('display','none');
 	})
 
+
+	var speed = 600;//滚动速度
+
+	var h=document.body.clientHeight;
+
+    //回到顶部
+
+    $("#toTop").click(function () {
+
+	  	$('html,body').animate({
+
+			scrollTop: '0px'
+
+		},
+
+		speed);			
+
+    });
+
+    $(document).scroll(function(){
+        var top = $(document).scrollTop();
+        if(top > 500)
+            $("#toTop").show();
+        else{
+        	$("#toTop").hide();
+        }
+    });
 	
 });
